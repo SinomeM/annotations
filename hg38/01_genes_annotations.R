cafa813954203d347a3b8116c770283d8261a057
Simone Montalbano <simone.montalbano@pm.me>
2025-03-19 15:25:46 +0000

    up

--- hg38/01_genes_annotations.R
+++ hg38/01_genes_annotations.R
@@ -30,3 +30,33 @@ fwrite(dt[transcript_ID %chin% all_trans[refseq_ID != '', transcript_ID], ][
 dt <- unique(gnomad[, .(gene, gene_id, transcript, lof.oe_ci.upper)])
 setnames(dt, 'lof.oe_ci.upper', 'LOEUF')
 fwrite(dt, './processed/gene_contrain_gnomad.txt', sep = '\t')
+
+
+
+
+# New version
+
+gnomad <- fread('./raw/gnomad.v4.1.constraint_metrics.tsv')[, .(gene_id, transcript, lof.oe_ci.upper)]
+colnames(gnomad) <- c('ens_ID', 'trans_ID', 'LOEUF')
+all_genes <- fread('./raw/hg38_biomart_all_genes.txt')
+colnames(all_genes) <- c('ens_ID', 'start', 'end', 'chr', 'biotype', 'hgnc_symbol')
+fread('./raw/genes_coords.txt')
+gt_biotypes <- fread('./raw/gene_trans_biotypes.txt')
+colnames(gt_biotypes) <- c('ens_ID', 'trans_ID', 'trans_type', 'biotype')
+exons_coord <- fread('./raw/exons_coord.txt')
+colnames(exons_coord) <- c('trans_ID', 'exon_start', 'exon_end', 'exon_ID')
+gte_ids <- fread('./raw/gene_trans_exon_IDs.txt')
+
+
+dir.create('./processed_new')
+fwrite(gnomad[!is.na(LOEUF), ], './processed_new/genes_trans_loeuf.txt', sep = '\t')
+fwrite(all_genes[, .(ens_ID, hgnc_symbol, chr, start, end)],
+       './processed_new/all_genes.txt', sep = '\t')
+
+dt <- gt_biotypes[trans_type == biotype &
+                  biotype %in% c('lncRNA', 'miRNS', 'snRNA', 'misc_RNA', 'snoRNA',
+                                 'protein_coding'), .(ens_ID, trans_ID, biotype)]
+fwrite(dt, './processed_new/genes_trans_biotype_select.txt', sep = '\t')
+
+exons <- merge(exons_coord, dt[, .(ens_ID, trans_ID)])
+fwrite(exons[, .(trans_ID, exon_ID, exon_start, exon_end)], 'processed_new/exons_select.txt', sep = '\t')
